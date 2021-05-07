025972a92cf772cf199365bd1c076fc412fd4e38
Trey Love <theendurinpupil@gmail.com>
2021-05-07 14:05:10 -0700

    created daily question model

---
+++ models/dailyQuestion.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class DailyQuestion extends Model { }
+
+DailyQuestion.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        question: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+
+        //add a boolean for if its already been posted?
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'daily_question',
+    }
+)
+module.exports = DailyQuestion
