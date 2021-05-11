4f97462419f5bceab7e484457aa9da6ee8fa5ffa
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-05-10 19:46:07 -0700

    accept incoming etc

--- models/Test.js
+++ models/Test.js
@@ -4,19 +4,8 @@
 
 // class Test extends Model { }
 
-<<<<<<< HEAD
-Test.init(
-   
-    
-
-
-
-
-)
-=======
 // Test.init(
 //     {
 
 //     }
 // );
->>>>>>> ef0f495402fb81c2245a80a3a4bd4bfb7a421963
