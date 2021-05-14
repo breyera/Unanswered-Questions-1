d077262bea7ecd86e7a7ebc2a2a802ca48ff61ef
Alicia Breyer <alicia_breyer@yahoo.com>
2021-05-14 16:45:45 -0700

    finished suggestions handlebars and js

---
+++ public/js/suggestions.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    $('select')
+        .change(function () {
+            $(this)
+                .find('option:selected')
+                .each(function () {
+                    var optionValue = $(this).attr('value');
+                    if (optionValue) {
+                        $('.box')
+                            .not('.' + optionValue)
+                            .hide();
+                        $('.' + optionValue).show();
+                    } else {
+                        $('.box').hide();
+                    }
+                });
+        })
+        .change();
+});
+
+document.querySelector('#suggestion').addEventListener('click', async (e) => {
+    e.preventDefault();
+    const name = document.querySelector('#phil-name').value;
+    const quote = document.querySelector('#quote').value;
+    const question = document.querySelector('#quest').value;
+    const qphilname = document.querySelector('#q-phil-name').value;
+
+    await fetch('/api/suggestion', {
+        method: 'POST',
+        body: JSON.stringify({
+            name,
+            quote,
+            question,
+            qphilname,
+        }),
+    });
+});
