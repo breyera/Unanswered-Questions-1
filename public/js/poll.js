82c0e1fb5c5ed2b09d42b62c07874d134b70b3a2
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-05-14 11:43:31 -0700

    fix percent display

--- public/js/poll.js
+++ public/js/poll.js
@@ -1,42 +1,87 @@
 const makePercent = (num1, num2) => {
-    return Math.round(100*((num1)/(num1 + num2))?((num1)/(num1 + num2)):0);
-}
+    return Math.round(100 * ((num1*1) / ((num1*1) + (num2*1))) ? 100* ((num1*1) / ((num1*1) + (num2*1))) : 0);
+};
 
-document.querySelectorAll('.yes-btn').forEach(n => {
-    n.addEventListener('click', async e => {
+document.querySelectorAll('.yes-btn').forEach((n) => {
+    n.addEventListener('click', async (e) => {
         const target = e.currentTarget;
 
-        const res = await fetch('/api/polls/' + target.getAttribute('data-poll-id'), {
-            method: 'PUT',
-            body: {
-                vote_yes: ((target.getAttribute('data-yes')*1) + 1)?((target.getAttribute('data-yes')*1) + 1):1,
-
+        const res = await fetch(
+            '/api/polls/' + target.getAttribute('data-poll-id'),
+            {
+                method: 'PUT',
+                body: {
+                    vote_yes:
+                        target.getAttribute('data-yes') * 1 + 1
+                            ? target.getAttribute('data-yes') * 1 + 1
+                            : 1,
+                },
             }
-        });
+        );
 
         console.log(res);
 
         const parent = target.parentNode;
 
+        const yes = target.getAttribute('data-yes') * 1 + 1;
+        const no = target.nextElementSibling.getAttribute('data-no') * 1;
+
+        const yesPercent = makePercent(yes, no);
+        const noPercent = makePercent(no, yes);
+
         parent.classList.remove('flex');
         parent.classList.add('hidden');
-        parent.nextSibling.classList.remove('hidden');
-        parent.nextSibling.classList.add('flex');
-    })
+        parent.nextElementSibling.firstElementChild.firstElementChild.setAttribute(
+            'style',
+            `width:${yesPercent}%`
+        );
+        parent.nextElementSibling.lastElementChild.firstElementChild.setAttribute(
+            'style',
+            `width:${noPercent}%`
+        );
+        parent.nextElementSibling.classList.remove('hidden');
+        parent.nextElementSibling.classList.add('flex');
+    });
 });
 
-document.querySelectorAll('.no-btn').forEach(n => {
-    n.addEventListener('click', async e => {
+document.querySelectorAll('.no-btn').forEach((n) => {
+    n.addEventListener('click', async (e) => {
         const target = e.currentTarget;
 
-        const res = await fetch('/api/polls/' + target.getAttribute('data-poll-id'), {
-            method: 'PUT',
-            body: {
-                vote_no: ((target.getAttribute('data-no')*1) + 1)?((target.getAttribute('data-yes')*1) + 1):1,
-
+        const res = await fetch(
+            '/api/polls/' + target.getAttribute('data-poll-id'),
+            {
+                method: 'PUT',
+                body: {
+                    vote_no:
+                        target.getAttribute('data-no') * 1 + 1
+                            ? target.getAttribute('data-yes') * 1 + 1
+                            : 1,
+                },
             }
-        });
+        );
 
         console.log(res);
-    })
+
+        const parent = target.parentNode;
+
+        const yes = target.previousElementSibling.getAttribute('data-yes') * 1;
+        const no = target.getAttribute('data-no') * 1 + 1;
+
+        const yesPercent = makePercent(yes, no);
+        const noPercent = makePercent(no, yes);
+
+        parent.classList.remove('flex');
+        parent.classList.add('hidden');
+        parent.nextElementSibling.firstElementChild.firstElementChild.setAttribute(
+            'style',
+            `width:${yesPercent}%`
+        );
+        parent.nextElementSibling.lastElementChild.firstElementChild.setAttribute(
+            'style',
+            `width:${noPercent}%`
+        );
+        parent.nextElementSibling.classList.remove('hidden');
+        parent.nextElementSibling.classList.add('flex');
+    });
 });
