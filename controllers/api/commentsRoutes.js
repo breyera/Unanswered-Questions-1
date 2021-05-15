d27a88576e53f76dde863fe33731bd11bcbdb468
Trey Love <theendurinpupil@gmail.com>
2021-05-14 20:09:46 -0700

    made slight tweaks to comment route

--- controllers/api/commentsRoutes.js
+++ controllers/api/commentsRoutes.js
@@ -5,16 +5,16 @@ const withAuth = require('../../utils/auth');
 router.post('/', withAuth, async (req, res) => {
     try {
         const newComment = await Comments.create({
-            ...req.body,
             user_id: req.session.user_id,
             daily_id: req.body.daily_id,
             comment: req.body.comment
         });
         // it will return to the commented post
-        res.redirect(req.header('Referrer'));
+
         res.status(200).json(newComment);
     } catch (err) {
         res.status(500).json(err);
+        console.error(err)
     }
 });
 
